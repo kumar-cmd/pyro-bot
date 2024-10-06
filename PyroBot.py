2c9961a2e9a958fc73c758fe432f7d25158dfcaa
kumar-cmd <61902175+kumar-cmd@users.noreply.github.com>
2024-10-07 01:09:07 +0530

    Add files via upload

--- PyroBot.py
+++ PyroBot.py
@@ -1,7 +1,12 @@
 from pyrogram import Client, filters
 
 # Replace 'API_ID' and 'API_HASH' with your actual values
-app = Client("my_bot", api_id="API_ID", api_hash="API_HASH")
+bot_token = '6850047081:AAGYb8GOGhNrYyN4tRCtXmRrOOHtxVVjXBg'
+api_id = 27903743
+api_hash = "aff31ed425563fcc8f5fc1afd4410d98"
+
+# Create the Pyrogram client
+app = Client("pyrobot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
 @app.on_message(filters.command("start"))
 def start(client, message):
