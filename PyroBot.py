d3f56d00fe934ddd0d2c69f5569e91af550adfa1
kumar-cmd <61902175+kumar-cmd@users.noreply.github.com>
2024-10-07 01:11:05 +0530

    Update PyroBot.py

--- PyroBot.py
+++ PyroBot.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, filters
 
 # Replace 'API_ID' and 'API_HASH' with your actual values
-bot_token = '6850047081:AAGYb8GOGhNrYyN4tRCtXmRrOOHtxVVjXBg'
-api_id = 27903743
-api_hash = "aff31ed425563fcc8f5fc1afd4410d98"
+bot_token = '6850047081:AAGYb8GOGhndbyN4tRCtXmdjdheHtxVVjXBg'
+api_id = 2645433743
+api_hash = "aff31ed425563fcc8f5fjdjdtd4410d98"
 
 # Create the Pyrogram client
 app = Client("pyrobot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
