fe7ef7785f80742c175bc81ba22c7f8567ca8780
kumar-cmd <61902175+kumar-cmd@users.noreply.github.com>
2024-10-07 03:11:47 +0530

    Update PyroBot.py

--- PyroBot.py
+++ PyroBot.py
@@ -159,5 +159,6 @@ async def members_invited(client, message):
 
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
-    app.run(port=port)
+    # port = int(os.environ.get("PORT", 5000))  # Use the PORT environment variable
+    # app.run(port=port)
+    app.run()
